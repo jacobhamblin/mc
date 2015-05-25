88d1bbaa6418abf54bb964a6b420798b8e67d1d3
lawlerblader <jacob@hamblin.cc>
2015-05-24 21:45:41 -0700

    nav menu works, more styling to come

--- app/assets/javascripts/components/App.js.jsx
+++ app/assets/javascripts/components/App.js.jsx
@@ -4,6 +4,7 @@ var App = React.createClass({
   render: function () {
     $('.menu-icon').click(function() {
       $(this).toggleClass('is-active');
+      $(".nav-menu").animate({width:'toggle'},350);
     });
 
     return (
