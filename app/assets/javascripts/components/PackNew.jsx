d58cc55f8cbf84e0b08b24993a6a028b7796afc6
lawlerblader <jacob@hamblin.cc>
2015-06-03 21:23:58 -0700

    update field spacing

--- app/assets/javascripts/components/PackNew.jsx
+++ app/assets/javascripts/components/PackNew.jsx
@@ -9,15 +9,15 @@ var PackNew = React.createClass({
         <div className='packnew-background' />
         <div className='form-container'>
           <form classname='newpack-form' action='api/packs' method='POST'>
-            <input type="text" name="pack[title]" placeholder="Title" className="ffield pack title" tabindex="1" />
+            <input type="text" name="pack[title]" placeholder="Title" className="ffield pack title" tabindex="1" /><br />
 
-            <input type="text" name="pack[description]" placeholder="Description" className="ffield pack description" tabindex="2" />
+            <input type="text" name="pack[description]" placeholder="Description" className="ffield pack description" tabindex="2" /><br />
 
-            <input type="text" name="pack[url]" placeholder="Download Url" className="ffield pack url" tabindex="3" />
+            <input type="text" name="pack[url]" placeholder="Download Url" className="ffield pack url" tabindex="3" /><br />
 
-            <input type="text" name="pack[prev]" placeholder="Preview Image Url" className="ffield pack prev" tabindex="4" />
+            <input type="text" name="pack[prev]" placeholder="Preview Image Url" className="ffield pack prev" tabindex="4" /><br />
 
-            <input type="submit" value="Create Pack" className="submit pack" tabindex="5" />
+            <input type="submit" value="Create Pack" className="submit pack" tabindex="5" /><br />
           </form>
         </div>
       </div>
