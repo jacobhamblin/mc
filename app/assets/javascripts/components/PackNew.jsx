d4cafe2f2333129cda9ae6e1590acbbbfce37d8e
lawlerblader <jacob@hamblin.cc>
2015-06-11 12:55:02 -0700

    some visuals on new pack

--- app/assets/javascripts/components/PackNew.jsx
+++ app/assets/javascripts/components/PackNew.jsx
@@ -6,20 +6,26 @@ var PackNew = React.createClass({
       <div className='packnew'>
         <div class="errors newpack-errors">
         </div>
-        <div className='packnew-background' />
-        <div className='form-container'>
-          <form classname='newpack-form' action='api/packs' method='POST'>
-            <input type="text" name="pack[title]" placeholder="Title" className="ffield pack title" tabindex="1" /><br />
+        <div className='packnew-background' style={{backgroundImage: window.bg}} />
+        <div className='form-container-outer'>
+          <div className='form-container'>
+            <div className='form-title'>
+              <h4>Minecraft</h4>
+              <h3>Customizer</h3>
+            </div>
+            <form classname='newpack-form' action='api/packs' method='POST'>
+              <input type="text" name="pack[title]" placeholder="Title" className="ffield pack title" tabindex="1" /><br />
 
-            <input type="text" name="pack[description]" placeholder="Description" className="ffield pack description" tabindex="2" /><br />
+              <textarea name="pack[description]" placeholder="Description" className="ffield pack description" tabindex="2" /><br />
 
-            <input type="text" name="pack[url]" placeholder="Download Url" className="ffield pack url" tabindex="3" /><br />
+              <input type="text" name="pack[url]" placeholder="Download Url" className="ffield pack url" tabindex="3" /><br />
 
-            <input type="text" name="pack[prev]" placeholder="Preview Image Url" className="ffield pack prev" tabindex="4" /><br />
+              <input type="text" name="pack[prev]" placeholder="Preview Image Url" className="ffield pack prev" tabindex="4" /><br />
 
-            <input type="submit" value="Create Pack" className="submit pack" tabindex="5" /><br />
-          </form>
-        </div>
+              <input type="submit" value="Create Pack" className="submit pack" tabindex="5" /><br />
+            </form>
+          </div>
+        </div>  
       </div>
 
     )
